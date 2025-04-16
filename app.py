9f641c8cb0925e9eed9fefde01d2f1fe2441fa8b
pedro <pedrobrancoh@gmail.com>
2025-04-15 22:25:03 -0300

    Adiciona dependências essenciais

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, UploadFile, File
+from processador import buscar_imagem_semelhante
+import shutil
+
+app = FastAPI()
+
+@app.post("/comparar")
+async def comparar(file: UploadFile = File(...)):
+    caminho_temp = f"temp/{file.filename}"
+    with open(caminho_temp, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    resultado = buscar_imagem_semelhante(caminho_temp)
+    return {"mais_semelhante": resultado}
