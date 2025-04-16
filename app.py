8c88605782b19c9047b7764268c0a69566f932c8
pedro <pedrobrancoh@gmail.com>
2025-04-16 09:57:08 -0300

    oi

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 from fastapi import FastAPI, UploadFile, File
 from processador import buscar_imagem_semelhante
 import shutil
+
 app = FastAPI()
 
 @app.post("/comparar")
 async def comparar(file: UploadFile = File(...)):
     caminho_temp = f"temp/{file.filename}"
+    
     with open(caminho_temp, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     
     resultado = buscar_imagem_semelhante(caminho_temp)
-    return {"mais_semelhante": resultado}
+    
+    return resultado
